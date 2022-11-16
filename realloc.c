d592b99655906074beccdbb62a772ba106a43b4e
madokscharles <charlesmadokes@gmail.com>
2022-11-16 15:44:52 -0800

    Reallocates a block of memory

---
+++ realloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _realloc - reallocates a block of memory
+ * @ptr: pointer
+ * @old_size: byte size of previous block
+ * @new_size: byte size of new block
+ *
+ * Return: pointer.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
