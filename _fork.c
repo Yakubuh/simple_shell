2ec65c7f51959649a617666ea261f952b9f955da
Bayulu Yakubu <105476695+Yakubuh@users.noreply.github.com>
2022-11-16 20:48:30 +0000

    Create _fork.c

---
+++ _fork.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * fork_cmd - forks an exec thread to run cmd
+ * @info: info struct
+ */
+
+void fork_cmd(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}
