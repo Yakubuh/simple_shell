97d28a78c84bddbffa9d9004e001818209b9a694
madokscharles <charlesmadokes@gmail.com>
2022-11-16 15:52:12 -0800

    Function keeps shell in loop

---
+++ shell_loop.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * hsh - keeps shell in loop
+ * @info: info struct
+ * @av: argument vector
+ *
+ * Return: 0 always on success
+ * 1 on error
+ */
+
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int builtin_ret = 0;
+
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("(Simple_shell)$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
