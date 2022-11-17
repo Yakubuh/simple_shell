6ef2785b0b331ee2f133191432985f23288b6d1f
madokscharles <charlesmadokes@gmail.com>
2022-11-16 16:49:15 -0800

    Betty corrections

--- more_lists.c
+++ more_lists.c
@@ -23,7 +23,7 @@ size_t list_len(const list_t *h)
  * list_to_strings - returns an array of strings of the list->str
  * @head: pointer to first node
  *
- * eturn: array of strings
+ * Return: array of strings
  */
 
 char **list_to_strings(list_t *head)
